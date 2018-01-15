22eb7429d263e99c5d8d0b94f9f3414f8fe5aaba
Luis Publiherz <publiherzseo@gmail.com>
2018-01-15 13:14:28 -0500

    Se sube la actualización del NAVBAR de perfil.

--- src/app/shared/topbar/topbar.component.ts
+++ src/app/shared/topbar/topbar.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ROUTES } from './topbar-routes.config';
-import { RouteInfo } from "./topbar.metadata";
-import { Router, ActivatedRoute } from "@angular/router";
+import { RouteInfo } from './topbar.metadata';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AuthService } from '../../core/auth/auth.service';
 
 declare var $: any;
 @Component({
@@ -14,12 +15,17 @@ declare var $: any;
 export class TopbarComponent implements OnInit {
     public menuItems: any[];
 
+    constructor(
+        private acAuth: AuthService,
+    ) { }
 
     ngOnInit() {
-        //$.getScript('./assets/js/app-sidebar.js');
+        // $.getScript('./assets/js/app-sidebar.js');
         this.menuItems = ROUTES.filter(menuItem => menuItem);
+    }
 
-        
+    onLogout() {
+        this.acAuth.logout();
     }
 
 }
